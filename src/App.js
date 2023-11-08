90ed8684d2cdafd17eb3116890b253bf6c82d872
Shubham Kumar <shubhamkr15060@gmail.com>
2023-11-08 16:57:40 +0530

    second method to create routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from "./pages/About";
 import Items from "./pages/Items";
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element:<Home/>
-    },
-    {
-      path: '/home',
-      element:<Home/>,
-    },
-    {
-      path: '/about',
-      element:<About/>
-    },
-    {
-      path: '/items',
-      element:<Items/>
-    }
-  ])
+  const routes = createRoutesFromElements(
+    <>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/items' element={<Items/>}/>
+    </>
+  )
+
+  const router = createBrowserRouter(routes)
+
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element:<Home/>
+  //   },
+  //   {
+  //     path: '/home',
+  //     element:<Home/>,
+  //   },
+  //   {
+  //     path: '/about',
+  //     element:<About/>
+  //   },
+  //   {
+  //     path: '/items',
+  //     element:<Items/>
+  //   }
+  // ])
 
   return (
     <>
